e44fe1b163d1fccdb162054e7723574bcc04a66c
Pietro Zardini <pizardini@gmail.com>
2024-08-05 15:10:08 -0300

    melhoria na página de detalhes da notícia

--- app/(system)/news/[id]/page.js
+++ app/(system)/news/[id]/page.js
@@ -2,9 +2,13 @@
 import { useEffect, useState } from 'react';
 import { CommentsFromNews, AddReaction, ListByNews } from './api';
 import { usePathname } from 'next/navigation';
-import { Button } from 'flowbite-react';
+import { Button, Spinner } from 'flowbite-react';
 import { Obtain } from '../api';
+import { CommentContext } from '../../comment/context';
+import { Card } from "flowbite-react";
+
 import Link from 'next/link';
+import NewCommentReader from './new';
 
 export default function NewsDetails({ usuario }) {
     const pathname = usePathname();
@@ -16,6 +20,8 @@ export default function NewsDetails({ usuario }) {
     const [dislikes, setDislikes] = useState(0);
     const [busy, setBusy] = useState(false);
     const [primeiroAcesso, setPrimeiroAcesso] = useState(true);
+    const [update, setUpdate] = useState(true);
+    const [modalOpen, setModalOpen] = useState(true);
 
     useEffect(() => {
         if (primeiroAcesso) {
@@ -23,6 +29,10 @@ export default function NewsDetails({ usuario }) {
             setPrimeiroAcesso(false);
         }
     }, [primeiroAcesso]);
+
+    const closeModals = () => {
+        setOperation({ id: null, action: null})
+    }
 
     useEffect(() => {
         if (id) {
@@ -80,16 +90,32 @@ export default function NewsDetails({ usuario }) {
 
     return (
         <div>
-            {busy ? <p>Carregando...</p> : 
+            {busy ? <Spinner /> : 
             news ? (
                 <>
-                    <h1>{news.headline}</h1>
-                    <p>{news.subhead}</p>
-                    <p>{new Date(news.publicationDateTime).toLocaleString('pt-BR')}</p>
-                    <p>{news.text}</p>
+                    <div className="flex justify-center items-center w-screen">
+                    <Card className="max-w-sm w-full">
+                        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {news.headline}
+                        </h2>
+                        <h5 className="text-2xl font-normal tracking-tight text-gray-900 dark:text-white">
+                        {news.subhead}
+                        </h5>
+                        <p className="text-gray-900 dark:text-white">{new Date(news.publicationDateTime).toLocaleString('pt-BR')}</p>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
+                        {news.text}
+                        </p>
+                        <p className="text-gray-900 dark:text-white">Likes: {likes} Dislikes: {dislikes}</p>
+                        <div className="flex gap-2">
+                        <Button onClick={() => handleReaction(1)}>Like</Button>
+                        <Button onClick={() => handleReaction(2)}>Dislike</Button>
+                        </div>
+                    </Card>
+                    </div>
 
                     {comments.length > 0 ? (
                         <div>
+
                             <h2>Comentários:</h2>
                             <ul>
                                 {comments.map(comment => (
@@ -107,15 +133,10 @@ export default function NewsDetails({ usuario }) {
                         <p>Sem comentários.</p>
                     )}
 
-                    <div>
-                        <Button onClick={() => handleReaction(1)}>Like</Button>
-                    </div>
-                    <div>
-                        <Button onClick={() => handleReaction(2)}>Dislike</Button>
-                    </div>
-
-                    <p>Likes: {likes}</p>
-                    <p>Dislikes: {dislikes}</p>
+                    <CommentContext.Provider value={{update: setUpdate, close: closeModals, newsId: id, userId: usuario.id }}>
+                        <NewCommentReader />
+                        {/* {modal} */}
+                    </CommentContext.Provider>
 
                     <Button as={Link} href="/" color="gray">
                         Voltar
